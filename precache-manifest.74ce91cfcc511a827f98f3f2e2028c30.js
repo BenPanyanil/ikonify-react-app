22aa680cca5a6ab5f5bc7b8d69f83d4e261633ae
BenPanyanil <benbackman54@gmail.com>
2020-08-07 12:36:29 +0300

    Updates

---
+++ precache-manifest.74ce91cfcc511a827f98f3f2e2028c30.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369d5bc36a2179ed55c4dfe5d539488f",
+    "url": "/ikonify-react-app/index.html"
+  },
+  {
+    "revision": "7bc6f4667f807332371a",
+    "url": "/ikonify-react-app/static/js/2.4e1df62d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ikonify-react-app/static/js/2.4e1df62d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4984357303cd0c6c8459",
+    "url": "/ikonify-react-app/static/js/main.aea79189.chunk.js"
+  },
+  {
+    "revision": "532d6a567819a2b03ade",
+    "url": "/ikonify-react-app/static/js/runtime-main.1491b0da.js"
+  },
+  {
+    "revision": "8de9fbca437535e6cf47b31223cca0f2",
+    "url": "/ikonify-react-app/static/media/footer-img.8de9fbca.mp4"
+  },
+  {
+    "revision": "9125c0fc0f80995d5efc9111a4739fc1",
+    "url": "/ikonify-react-app/static/media/ikonifyBasicWhite.9125c0fc.svg"
+  },
+  {
+    "revision": "3a88bf5affba24336ce774ed7d7bbf2f",
+    "url": "/ikonify-react-app/static/media/ikonifyStandAlone.3a88bf5a.svg"
+  },
+  {
+    "revision": "1bbfa8a3cc32dc3cc5093ec0097b1d82",
+    "url": "/ikonify-react-app/static/media/kx-treeshears-FrontPage.1bbfa8a3.png"
+  },
+  {
+    "revision": "5c3e82758bc0bced3af94dd17c933212",
+    "url": "/ikonify-react-app/static/media/kx-treeshears-domainimg.5c3e8275.svg"
+  },
+  {
+    "revision": "1e72cea455068c5804af719af62e9103",
+    "url": "/ikonify-react-app/static/media/play-icon.1e72cea4.svg"
+  },
+  {
+    "revision": "786bbfe7c47d797b7bb135610394ef15",
+    "url": "/ikonify-react-app/static/media/section2_Hero-img.786bbfe7.jpg"
+  }
+]);
